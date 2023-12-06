849c33c5ac8d2a621cd34062e44b9f7bf5b08b64
Mazo Vasiliy <mazo@protonmail.ch>
2023-12-07 01:06:23 +0300

    formatting

--- 2023/aoc23_06.clj
+++ 2023/aoc23_06.clj
@@ -12,9 +12,10 @@ Distance:  9  40  200")
   (BigInteger. s))
 
 (defn roots
-  "Считает минимальный и максимальный целые корни неравенства $$ x^2 - xt + S < 0 $$"
+  "Считает минимальный и максимальный целые корни неравенства $$ x^2 - xt + S < 0 $$."
   [[t s]]
-  (map (fn [[round-fn add-fn]] (round-fn (* 0.5 (add-fn t (m/sqrt (- (* t t) (* 4 s)))))))
+  (map (fn [[round-fn add-fn]]
+         (round-fn (* 0.5 (add-fn t (m/sqrt (- (* t t) (* 4 s)))))))
        [[(comp inc int m/floor) -] [(comp dec int m/ceil) +]]))
 
 (defn count-whole-solutions
@@ -23,13 +24,20 @@ Distance:  9  40  200")
 
 (->> (str/split-lines data)
      (map (fn [line]
-            (map parse-int (-> line (str/split #":\s*") second (str/split #"\s+")))))
+            (map parse-int (-> line
+                               (str/split #":\s*")
+                               second
+                               (str/split #"\s+")))))
      (apply mapv vector)
      (map (comp count-whole-solutions roots))
      (reduce *))
 
 (->> (str/split-lines data)
      (map (fn [line]
-            (-> line (str/split #":\s*") second (str/split #"\s+") str/join parse-int)))
+            (-> line
+                (str/split #":\s*")
+                second
+                (str/split #"\s+")
+                str/join parse-int)))
      roots
      count-whole-solutions)
